22fc84636d4ed37e870383a0743d6beff30cd3f4
Shubhankar Singh <shubhankar88600@gmail.com>
2026-08-09 14:29:40 +0530

    Clarify SQLAlchemy declarative base

--- app/db/base.py
+++ app/db/base.py
@@ -1,9 +1,7 @@
-# Register every SQLAlchemy model.
-
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    """Base class for all SQLAlchemy ORM models."""
+    """Shared declarative base for all SQLAlchemy models."""
 
     pass
